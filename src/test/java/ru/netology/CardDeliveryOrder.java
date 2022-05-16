629254be7b1cd015692c2f3da656459349649585
Bolot Gonchikov <gonchikovbb@gmail.com>
2022-05-16 12:51:38 +0700

    Create

---
+++ src/test/java/ru/netology/CardDeliveryOrder.java
@@ -0,0 +1,18 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+class CardDeliveryOrder {
+    @Test
+    void shouldCardDeliveryOrder() {
+
+    }
+
+
+}
